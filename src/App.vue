200efcbebd64fc8f08ec333a2adfabeb97363fa8
MarinaLasorsa <marina.lasorsa@gmail.com>
2024-04-22 15:54:10 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+  components: {
+    AppHeader,
+    AppFooter,
+  },
+  data() {
+    return {
+
+    }
+  }
+
+}
+</script>
+
+<template>
+  <div>
+
+  </div>
+</template>
+
+<style lang="scss">
+@use 'bootstrap';
+@use './styles/general';
+</style>
